d521dbce1cec28419b7ff7b932a5bf005c675da9
andreeaszabo10 <121065656+andreeaszabo10@users.noreply.github.com>
2025-03-10 14:46:18 +0200

    Update transform2D.h

--- transform2D.h
+++ transform2D.h
@@ -8,7 +8,7 @@ namespace transform2D
     // Translate matrix
     inline glm::mat3 Translate(float translateX, float translateY)
     {
-        // TODO(student): Implement the translation matrix
+        
         return glm::transpose(
             glm::mat3(1, 0, translateX,
                 0, 1, translateY,
@@ -20,7 +20,7 @@ namespace transform2D
     // Scale matrix
     inline glm::mat3 Scale(float scaleX, float scaleY)
     {
-        // TODO(student): Implement the scaling matrix
+       
         return glm::transpose(
             glm::mat3(scaleX, 0, 0,
                 0, scaleY, 0,
@@ -32,7 +32,6 @@ namespace transform2D
     // Rotate matrix
     inline glm::mat3 Rotate(float radians)
     {
-        // TODO(student): Implement the rotation matrix
         return glm::transpose(
             glm::mat3(cos(radians), -sin(radians), 0,
                 sin(radians), cos(radians), 0,
